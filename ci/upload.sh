f4c7ab04d5c62898675cd4b43ac4fc4c314cdd5d
sawanoboly <sawanoboriyu@higanworks.com>
2016-09-08 12:48:00 +0900

    use ARTIFACT PATH

--- ci/upload.sh
+++ ci/upload.sh
@@ -5,4 +5,4 @@ aws s3api put-object --acl public-read \
   --region ${AWS_REGION} \
   --bucket ${S3_BUCKET} \
   --key ${S3_KEY_PREFIX}/wordpress.zip \
-  --body wordpress.zip
+  --body ${CIRCLE_ARTIFACTS}/wordpress.zip
